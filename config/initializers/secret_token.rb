9e95d96340b81ee885b76226872ce7ec23a7c754
sravanthi-kl <sravanthi.kl@strata.co.in>
2015-08-06 13:23:20 +0530

    file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleKpi::Application.config.secret_token = '44d4955fa6a72f069b3277d1270de35a639e25742c0618e286d31d82dc80f900b320ff1ac6033fd4ea886dbcb7e46489e52523e349d4464e9709b60b9cca64a7'
